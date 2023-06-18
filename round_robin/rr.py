9ff1653df483cc4a58325cc822bc7a77028f7f33
Pricylia <pricylia884@gmail.com>
2023-06-18 20:31:36 -0300

    update

--- round_robin/rr.py
+++ round_robin/rr.py
@@ -1,48 +1,56 @@
-def circular(fila_processos, quantum, troca_contexto):
-    turn_around = [0] * len(fila_processos)
+import matplotlib.pyplot as plt
 
-    tempo_atual = 0
-    while True:
-        if sum (fila_processos) <= 0:
-            print("Todos os processos foram executados!")
-            break
-        else:
-            for i in range(len(fila_processos)):
-                if fila_processos[i] <= 0:
-                    print(f"\nP{i} Já Finalizado!")
-                elif fila_processos[i] <= quantum:
-                    tempo_atual += fila_processos[i]
-                    fila_processos[i] -= fila_processos[i]
+# Definindo as tarefas
+tarefas = [
+    {"ingresso": 5, "duracao": 30, "prioridade": 4},
+    {"ingresso": 15, "duracao": 10, "prioridade": 2},
+    {"ingresso": 10, "duracao": 40, "prioridade": 1},
+    {"ingresso": 0, "duracao": 20, "prioridade": 3}
+]
 
-                    print(f"\nP{i} executa")
-                    print(f"Termino em T-{tempo_atual}")
-                    if fila_processos[i] == 0:
-                        turn_around[i] = tempo_atual
-                        print(f"Processo P{i} terminou em T-{tempo_atual}")
-                        print("Troca de Contexto")
-                        tempo_atual += troca_contexto
-                else:
-                    tempo_atual += quantum
-                    fila_processos[i] -= quantum
+# Configurações do escalonamento
+quantum = 15
+troca_contexto = 4
 
-                    print(f"\nP{i} executa")
-                    print(f"Termino em T-{tempo_atual}")
-                    print("Troca de Contexto")
-                    tempo_atual += troca_contexto
-                    turn_around[i] = tempo_atual
-            print("-" * 30)
+# Executando o escalonamento Round-Robin
+tempo_atual = 0
+tempos_espera = []
+tempos_vida = []
+tempos_execucao = []
 
-    print(f"Tempo onde terminou cada processo: {turn_around}")
-    return turn_around
+while tarefas:
+    tarefa_atual = tarefas.pop(0)
+    inicio_execucao = max(tarefa_atual["ingresso"], tempo_atual)
+    duracao_execucao = min(tarefa_atual["duracao"], quantum)
 
-def tempo_atual_medio_turnround(lista_processos, lista_tempo):
-    resultado = sum(lista_tempo) / (len(lista_processos))
-    print(f"Tempo Medio de Turnround = {resultado:.2f}")
+    tempos_execucao.append((inicio_execucao, duracao_execucao))
+    tempos_espera.append(inicio_execucao - tarefa_atual["ingresso"])
+    tempos_vida.append(duracao_execucao)
 
-if __name__ == '__main__':
-    fila_processos = [40, 20, 50, 30]
-    quantum = 20
-    troca_contexto = 5
+    tarefa_atual["duracao"] -= duracao_execucao
+    tempo_atual = inicio_execucao + duracao_execucao + troca_contexto
 
-    TurnAround = circular(fila_processos, quantum, troca_contexto)
-    tempo_atual_medio_turnround(fila_processos, TurnAround)
+    if tarefa_atual["duracao"] > 0:
+        tarefas.append(tarefa_atual)
+
+# Calculando os tempos médios de espera e de vida
+tempo_medio_espera = sum(tempos_espera) / len(tempos_espera)
+tempo_medio_vida = sum(tempos_vida) / len(tempos_vida)
+
+# Imprimindo os tempos médios
+print("Tempo médio de espera:", tempo_medio_espera)
+print("Tempo médio de vida:", tempo_medio_vida)
+
+# Plotando o gráfico da sequência de execução das tarefas
+fig, ax = plt.subplots()
+
+for i, (inicio, duracao) in enumerate(tempos_execucao):
+    ax.broken_barh([(inicio, duracao)], (10 * i, 9), facecolors='tab:blue')
+
+ax.set_xlabel('Tempo')
+ax.set_ylabel('Tarefa')
+ax.set_yticks([10 * i + 5 for i in range(len(tarefas))])
+ax.set_yticklabels([f'Tarefa {i+1}' for i in range(len(tarefas))])
+ax.grid(True)
+
+plt.show()
